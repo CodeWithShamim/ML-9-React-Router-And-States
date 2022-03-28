d18fd3c0d44637229dccae4fd2346714cbc22c36
codewithshamim <shamimislamonline@gmail.com>
2022-03-28 22:25:41 +0600

    lineChart added

--- Module 51 (React Bootstrap Tailwind Axios Rechart)/react-with-bootstrap/src/App.js
+++ Module 51 (React Bootstrap Tailwind Axios Rechart)/react-with-bootstrap/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import PieChart from './components/LineChart/LineChart';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <PieChart></PieChart>
     </div>
   );
 }
