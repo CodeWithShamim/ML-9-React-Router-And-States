2a93d42e67c6e6011efc66ab856d8f1cb16b3e71
codewithshamim <shamimislamonline@gmail.com>
2022-03-28 19:20:38 +0600

    navbar update & heroicon added

---
+++ Module 51 (React Bootstrap Tailwind Axios Rechart)/react-with-bootstrap/src/components/Link/Link.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Link = ({item}) => {
+    const {name,link} = item;
+    return (
+        <div>
+            <li className='ml-4 text-xl text-white hover:text-orange-500'><a href={link}>{name}</a></li>
+        </div>
+    );
+};
+
+export default Link;
--- Module 51 (React Bootstrap Tailwind Axios Rechart)/react-with-bootstrap/src/components/Navbar/Navbar.js
+++ Module 51 (React Bootstrap Tailwind Axios Rechart)/react-with-bootstrap/src/components/Navbar/Navbar.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Link from '../Link/Link';
+import './Navbar.css'
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 const Navbar = () => {
+    const [items, setItems] = useState(false);
+    const linkItem = [
+        {id:1, name: "Home", link:'./home'},
+        {id:1, name: "Products", link:'./products'},
+        {id:1, name: "Contact", link:'./contact'},
+        {id:1, name: "About", link:'./about'},
+        {id:1, name: "Account", link:'./account'}
+    ]
     return (
-        <nav className='bg-purple-300'>
+        <nav className='bg-purple-500'>
+            <div onClick={()=>setItems(!items)}  className='w-6 h-6 md:hidden'>
+                {items ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+            </div>
            <ul className='md:flex p-4 justify-center items-center'>
-               <li><a href="./Home">Home</a></li>
-               <li><a href="./Products">Products</a></li>
-               <li><a href="./Contact">Contact</a></li>
-               <li><a href="./About">About</a></li>
-               <li><a href="./Account">Account</a></li>
+               {linkItem.map(item=><Link key={item.id} item={item}></Link>)}
            </ul> 
         </nav>
     );
