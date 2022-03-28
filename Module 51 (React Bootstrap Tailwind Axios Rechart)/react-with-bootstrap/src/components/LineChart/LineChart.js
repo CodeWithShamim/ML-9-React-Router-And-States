49a83bc83c937bfeb8652354dd70011e94b75261
codewithshamim <shamimislamonline@gmail.com>
2022-03-28 22:34:04 +0600

    chart update

--- Module 51 (React Bootstrap Tailwind Axios Rechart)/react-with-bootstrap/src/components/LineChart/LineChart.js
+++ Module 51 (React Bootstrap Tailwind Axios Rechart)/react-with-bootstrap/src/components/LineChart/LineChart.js
@@ -48,15 +48,15 @@ const PieChart = () => {
       ]
     return (
         <div>
-            <LineChart width={730} height={250} data={data}
+            <LineChart width={1300} height={250} data={data}
                 margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                <CartesianGrid strokeDasharray="3 3" />
+                <CartesianGrid strokeDasharray="1 1" />
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                <Line type="monotone" dataKey="pv" stroke="red" />
+                <Line type="monotone" dataKey="uv" stroke="blue" />
             </LineChart>
         </div>
     );
