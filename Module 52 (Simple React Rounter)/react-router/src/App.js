abd571cb0c89ec5569792d324ea6ea0db072394f
codewithshamim <shamimislamonline@gmail.com>
2022-03-29 20:59:46 +0600

    react router component added

--- Module 52 (Simple React Rounter)/react-router/src/App.js
+++ Module 52 (Simple React Rounter)/react-router/src/App.js
@@ -1,14 +1,19 @@
+import {Route, Routes} from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 import Products from './components/Products/Products';
 
 function App() {
   return (
     <div className="App">
-       <Home></Home>
-       <Products></Products>
-       <About></About>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/products' element={<Products/>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
