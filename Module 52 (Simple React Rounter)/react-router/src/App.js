2447f6bd94d6b8356caad6a9e663019c91109bcc
codewithshamim <shamimislamonline@gmail.com>
2022-03-29 20:29:21 +0600

    react router added

--- Module 52 (Simple React Rounter)/react-router/src/App.js
+++ Module 52 (Simple React Rounter)/react-router/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Products from './components/Products/Products';
 
 function App() {
   return (
     <div className="App">
-       
+       <Home></Home>
+       <Products></Products>
+       <About></About>
     </div>
   );
 }
