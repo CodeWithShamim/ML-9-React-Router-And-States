8683436e2640beeba3afa45fa6a59813ac6bc26d
codewithshamim <shamimislamonline@gmail.com>
2022-03-29 21:24:07 +0600

    dynamic path added

--- Module 52 (Simple React Rounter)/react-router/src/components/Home/Home.js
+++ Module 52 (Simple React Rounter)/react-router/src/components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
 const Home = () => {
+    let navigate = useNavigate();
+    const showProductDetails = (id) =>{
+        const path = `/products/${id}`;
+        navigate(path)
+
+    }
     return (
         <div>
-            this is Home
+            <h1>this is Home</h1>
+            <button onClick={()=>showProductDetails(10)}>GO About Page</button>
         </div>
     );
 };
