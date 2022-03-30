fcd2e62d178f13b007cfed047fb5293d3938cfdc
codewithshamim <shamimislamonline@gmail.com>
2022-03-30 19:35:30 +0600

    load dynamically data

--- Module 52 (Simple React Rounter)/react-router/src/components/Products/Products.js
+++ Module 52 (Simple React Rounter)/react-router/src/components/Products/Products.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Products = () => {
-    const searchProducts = e =>{
-        console.log(e.target.value);
+    const [searchFood, setSearchFood] = useState("");
+    const [meals, setMeals] = useState([]);
+    const searchProduct = e =>{
+        setSearchFood(e.target.value)
     }
+
+
+    useEffect(()=>{
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchFood}`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.meals){
+                setMeals(data.meals)
+            }
+        })
+    },[searchFood])
+
+    console.log(meals);
     return (
         <div>
-            <h1>Search your favourite products</h1>
-            <input type="search" onChange={searchProducts} />
+            <h1>Search your favourite Foods</h1>
+            <input type="search" onChange={searchProduct} />
+            <h3>Result: {meals.length}</h3>
         </div>
     );
 };
