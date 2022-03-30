d2372dd5e11c6528294bc9da4a7a246137357444
codewithshamim <shamimislamonline@gmail.com>
2022-03-30 23:03:35 +0600

    load data dynamically

--- Module 52 (Simple React Rounter)/react-router/src/components/DynamicPart/DynamicPart.js
+++ Module 52 (Simple React Rounter)/react-router/src/components/DynamicPart/DynamicPart.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 
 const DynamicPart = () => {
-    const params = useParams();
-    console.log(params.id);
+    const {id} = useParams();
+    const [show, setShow] = useState([]);
+    useEffect(()=>{
+        const url = `https://jsonplaceholder.typicode.com/users/${id}`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>setShow(data));
+    },[id])
+    // console.log(id);
     return (
         <div>
             <h1>this is dynamic part:</h1>
-            <h5>dynamic url parameter: {params.id}</h5>
+            <h5>dynamic url parameter: {id}</h5>
+            <p>------------------------------</p>
+            <h1>ID: {show.id}</h1>
+            <h2>Name: {show.name}</h2>
+            <h2>Username: {show.username}</h2>
+            <h2>Email: {show.email}</h2>
+            <h2>Phone: {show.phone}</h2>
+            <h2>Website: {show.website}</h2>
         </div>
     );
 };
