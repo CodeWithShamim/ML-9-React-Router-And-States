31d049597cd28e3f3e02e301552a2d5254d51ab2
codewithshamim <shamimislamonline@gmail.com>
2022-04-01 15:24:23 +0600

    file added

--- Module 54 ( Router Deploy Context API and devtool)/react-context-api/src/components/Grandpa/Grandpa.js
+++ Module 54 ( Router Deploy Context API and devtool)/react-context-api/src/components/Grandpa/Grandpa.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Father from '../Father/Father'
 import Uncle from '../Uncle/Uncle'
 import Aunty from '../Aunty/Aunty'
+import './Grandpa.css'
 
 const Grandpa=() => {
     return (
-        <div>
+        <div className='grandpa'>
             <Father></Father>
             <Uncle></Uncle>
             <Aunty></Aunty>
         </div>
     );
 };
-
 export default Grandpa;
